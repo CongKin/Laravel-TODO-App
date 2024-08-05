4f02395b82d1e2a3f618a538fca454a0a7557413
Ong Cong Kin <congkinong0112@gmail.com>
2024-08-05 16:19:07 +0800

    Database initialization

---
+++ database/migrations/2024_08_05_080923_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->longText('task');
+            $table->char('status',1);
+            $table->char('priority',1);
+            $table->date('deadline')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
