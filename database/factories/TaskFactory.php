38fa8f281f86ce358803c396fc38261047d23232
Ong Cong Kin <congkinong0112@gmail.com>
2024-08-05 16:33:13 +0800

    create seed data

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'task' => fake()->realText(20),
+            'status' => '1',
+            'priority' => '1',
+            'deadline' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'user_id' => 1,
+        ];
+    }
+}
