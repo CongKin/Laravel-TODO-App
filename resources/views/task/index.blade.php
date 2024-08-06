3198696413334225ad7556d2551e6d2fd8c14c46
Ong Cong Kin <congkinong0112@gmail.com>
2024-08-06 12:36:43 +0800

    add action and operation at index

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -11,7 +11,8 @@
                     <th class="cell100 column3">Priority</th>
                     <th class="cell100 column4">Due Date</th>
                     <th class="cell100 column5">Creation Date</th>
-                    <th class="cell100 column6">Function</th>
+                    <th class="cell100 column6">Action</th>
+                    <th class="cell100 column7">Operation</th>
                 </tr>
             </thead>
             <tbody>
@@ -38,7 +39,24 @@
                         </td>
                         <td class="cell100 column4">{{ $task->deadline ? \Carbon\Carbon::parse($task->deadline)->format('Y-m-d') : 'No deadline' }}</td>
                         <td class="cell100 column5">{{ $task->created_at->format('Y-m-d') }}</td>
-                        <td class="cell100 column6">Function</td>
+                        <td class="cell100 column6">
+                            <form action="{{ route('task.show', $task) }}" method="POST" class="mt-3">
+                                @csrf
+                                @method('PATCH')
+                                @if($task->status == '1')
+                                    <button type="submit" name="status" value="2" class="btn btn-primary">Start Task</button>
+                                @elseif($task->status == 'In 2')
+                                    <button type="submit" name="status" value="3" class="btn btn-success">Complete Task</button>
+                                    <button type="submit" name="status" value="1" class="btn btn-warning">Set to Pending</button>
+                                @elseif($task->status == '3')
+                                    <button type="submit" name="status" value="2" class="btn btn-primary">Reopen Task</button>
+                                @endif
+                            </form>
+                        </td>
+                        <td class="cell100 column7">
+                            <a href="{{ route('task.show', $task) }}" class="task-edit-button">View</a>
+                            <a href="{{ route('task.edit', $task) }}" class="task-edit-button">Edit</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
